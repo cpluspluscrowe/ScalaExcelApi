32556e74387c6816635c099f8bd52e4992091b4d
cpluspluscrowe <ccrowe@unomaha.edu>
2017-08-17 10:00:21 -0500

    Changed Range and Value to functions, rather than attributes

--- src/main/scala/Range.scala
+++ src/main/scala/Range.scala
@@ -1,26 +1,39 @@
 import org.apache.poi.hssf.util.CellReference
-import org.apache.poi.xssf.usermodel.XSSFSheet
+import org.apache.poi.ss.usermodel.DataFormatter
+import org.apache.poi.xssf.usermodel.{XSSFCell, XSSFSheet}
 
 class Sheet extends XSSFSheet(){
-  private var Range:_Range = _;
+  protected var Range:_Range = _;
   def Range(address:String): _Range ={
     val cellReference:CellReference = new CellReference(address);
-    this.Range = new _Range(cellReference);
+    this.Range = new _Range(this,cellReference);
     return this.Range;
   }
   def Range(row:Int,column:Int): _Range ={
     val cellReference:CellReference = new CellReference(row,column);
-    this.Range = new _Range(cellReference);
+    this.Range = new _Range(this,cellReference);
     return this.Range;
   }
 }
 
-class _Range() {
-  var Value:String = _;
-  var Address:String = _;
-  var Column:Int = _;
-  var Row:Int = _;
-  def this(cellReference: CellReference){
-    this();
+class _Range(parent:Sheet,cellReference: CellReference) {
+  protected val formatter = new DataFormatter;
+  protected val Row = parent.getRow(cellReference.getRow);
+  if(Row == null){
+    parent.createRow(cellReference.getRow);
+  }
+  protected var _Cell:XSSFCell = Row.getCell(cellReference.getCol);
+  if(_Cell == null){
+    _Cell = Row.createCell(cellReference.getCol);
+  }
+
+  def Address(): String ={
+    val address:String = _Cell.getAddress.formatAsString();
+    return address;
+  }
+  def ValueString(): String ={
+    val formattedCellValue = formatter.formatCellValue(_Cell)
+    var value:String = formattedCellValue;
+    return value;
   }
 }
